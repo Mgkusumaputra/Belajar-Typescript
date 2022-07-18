f56ca8babe2caf2451a7c49645c34d22d102f678
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2022-07-18 19:50:40 +0700

    Any type

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,8 @@ console.log(age);
 let sales = 123_456_789;        /* If we have large number, we can separate its digits using an underscore. */
 let course = "Typescript";
 let is_published = true;
-let level;
+let level;                      /* If we declare a variable and dont initialize it, the typescript compiler assumes this variable is of type any */
+
+function render(document) {
+    console.log(document);      /* Implicity = we haven't explicity or clearly set the type of the parameter */
+}
