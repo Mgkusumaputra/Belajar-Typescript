1442a618ea51f9818267878f886fdbf9e4e2cc91
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2022-07-18 19:35:22 +0700

    Built-in types

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,10 @@ let age: number = 20;
 if (age < 50)
     age += 10;
 console.log(age);
+
+//
+
+let sales = 123_456_789;        /* If we have large number, we can separate its digits using an underscore. */
+let course = "Typescript";
+let is_published = true;
+let level;
