c26234d4b57543df3362be3704e60995a880adc0
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2022-07-22 07:53:33 +0700

    Functions

--- src/index.ts
+++ src/index.ts
@@ -39,3 +39,15 @@ let user: [number, string, boolean, number] = [1, "Mosh", true, 0];
 const enum Size { Small = 1, Medium, Large }      /* By default first member value will be set to 0. If we define the value with string, we must set a value for each member.*/
 let mySize: Size = Size.Medium;
 console.log(mySize);        /* if we define enum using const compiler will generated more optimize code  */
+
+
+
+// Functions
+
+function calculateTax(income: number, taxYear = 2022): number {         /* Always properly annotate functions */
+    if (taxYear < 2022) 
+        return income * 1.2;
+    return income * 1.3;
+}
+
+calculateTax(10_000);
