748997ac710004ffc106425bc2497da8f3ff46c6
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2022-07-22 08:08:14 +0700

    Type Aliases

--- src/index.ts
+++ src/index.ts
@@ -54,13 +54,17 @@ calculateTax(10_000);
 
 
 
-// Objects
+// Type Aliases
 
-let employee: {
+type Employee = {
     readonly id: number,
     name: string,
     retire: (date: Date) => void
-} = { 
+}
+
+// Objects (Connect To "Type Aiases")
+
+let employee: Employee = { 
     id: 1, 
     name: "Mosh",
     retire: (date: Date) => {
